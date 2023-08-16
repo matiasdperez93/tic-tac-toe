ae7a53cfaf94160ea095331f5a82fbe31023da47
matiasdperez <88059666+matiasdperez93@users.noreply.github.com>
2023-08-16 16:18:26 -0300

    tic-tac-toe en react

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+
+//components
+import Square from './components/Square';
+import Board from  './components/Board'
+
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Tic-Tac-Toe</h1>
+        <Board/>
+    </div>
+  );
+}
+
+export default App;
